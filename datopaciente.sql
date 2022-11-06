7cd64f1a5757ec263cae5c5ae74e547ddc799160
RANDAL440 <114038089+malavorti440@users.noreply.github.com>
2022-11-05 22:32:20 -0300

    Create datopaciente.sql

---
+++ datopaciente.sql
@@ -0,0 +1,7 @@
+CREATE TABLE datopaciente (
+  id INTEGER PRIMERY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  lastname TEXT NOT NULL,
+  telefono INTEGER NOT NULL
+  )
+  INSERT INTO datopaciente (name, lastname, telefono) VALUES("Andrina", "Albertti", 945182848)
